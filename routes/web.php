bf3500cf8e37051a659faee1ecbf9bde6ae9d695
Siam539 <kazi.siam08@gmail.com>
2026-04-29 12:49:20 +0200

    CHangement routes pour la page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -22,8 +24,27 @@ Route::middleware(['auth'])->group(function () {
 
 // Routes admin
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/products', [AdminController::class, 'adminProducts'])->name('products');
+    //products management
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+    Route::post('/products/update/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::get('/products/delete/{id}', [ProductController::class, 'delete'])->name('products.delete');
+    Route::get('/products/change-status/{id}', [ProductController::class, 'changeStatus'])->name('products.change-status');
+
+    //categories management
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::post('/categories/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::get('/categories/delete/{id}', [CategoryController::class, 'delete'])->name('categories.delete');
+
+    //orders management
     Route::get('/orders', [AdminController::class, 'adminOrders'])->name('orders');
+
+    //users management
     Route::get('/users', [AdminController::class, 'usersList'])->name('users');
     Route::get('/users/create', [AdminController::class, 'createUser'])->name('users.create');
     Route::post('/users/store', [AdminController::class, 'storeUser'])->name('users.store');
