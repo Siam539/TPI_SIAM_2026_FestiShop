dfebc42702737793fa5af8abbd96295ca880269e
Siam539 <kazi.siam08@gmail.com>
2026-05-06 10:10:21 +0200

    Ficher de données a jour pour le commancement su site

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
